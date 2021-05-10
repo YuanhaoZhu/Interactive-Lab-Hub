b46efcf287d6f5ed04d31ca2ecdfeb190eafbb72
Yuanhao Zhu <pandazhu333@gmail.com>
2021-05-10 03:56:38 -0400

    Update display.py

--- Final Project/display.py
+++ Final Project/display.py
@@ -1,11 +1,45 @@
-# Setup the Display
-import board
-import displayio
-import adafruit_hx8357
-displayio.release_displays()
-spi = board.SPI()
-tft_cs = board.CE0
-tft_dc = board.D25
- 
-display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs)
-display = adafruit_hx8357.HX8357(display_bus, width=800, height=480, backlight_pin=board.D18)
+#!/usr/bin/env python3
+
+import tkinter as tk
+
+# --- functions ---
+
+def on_escape(event=None):
+    print("escaped")
+    root.destroy()
+
+# --- main ---
+
+root = tk.Tk()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# --- fullscreen ---
+
+#root.overrideredirect(True)  # sometimes it is needed to toggle fullscreen
+                              # but then window doesn't get events from system
+#root.overrideredirect(False) # so you have to set it back
+
+root.attributes("-fullscreen", True) # run fullscreen
+root.wm_attributes("-topmost", True) # keep on top
+#root.focus_set() # set focus on window
+
+# --- closing methods ---
+
+# close window with key `ESC`
+root.bind("<Escape>", on_escape)
+
+# close window after 5s if `ESC` will not work
+root.after(5000, root.destroy) 
+
+# --- canvas ---
+
+canvas = tk.Canvas(root)
+canvas.pack(fill='both', expand=True)
+
+canvas.create_oval((0, 0, screen_width, screen_height), fill='red', outline='')
+
+# --- start ---
+
+root.mainloop()
